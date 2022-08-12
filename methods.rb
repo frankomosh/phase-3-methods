6e4a568c7a1c380f1ad2ab02243415b37f0b7e07
Frank-197 <frankomosh197@gmail.com>
2022-08-13 00:25:00 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer()
+   puts "Hello, programmer!"
+end 
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+ 
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+  
+def add(num1, num2)
+    return num1 + num2
+end    
+add(2,5)
+def halve(param)
+    if param.class != Integer
+        return nil
+    end
+
+    return param/2
+end
+halve(6)    
